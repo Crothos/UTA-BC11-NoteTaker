9cf29b7ade8ae4d79170840b55732940bae96371
Crothos <s.g.totten@gmail.com>
2023-01-31 22:51:09 -0600

    added wild card route

--- Develop/server.js
+++ Develop/server.js
@@ -14,11 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => res.json(noteData));
@@ -62,13 +62,17 @@ app.post('/api/notes', (req, res) => {
         status: 'success',
         body: newNote,
       };
-  
+
       console.log(response);
       res.status(201).json(response);
     } else {
       res.status(500).json('Error in posting note');
     }
   });
+
+  app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
