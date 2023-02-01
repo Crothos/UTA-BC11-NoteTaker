f9abc7929e6e1efc5b17a10123db3b6ff63ac759
Crothos <s.g.totten@gmail.com>
2023-01-31 20:54:10 -0600

    restarted repo

---
+++ Develop/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const noteData = require('./db/db.json')
+const uuid = require('./helpers/uuid');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.post('/api/reviews', (req, res) => {
+
+  console.info(`${req.method} request received to add a review`);
+
+  const { product, review, username } = req.body;
+
+  if (product && review && username) {
+
+    const newReview = {
+      product,
+      review,
+      username,
+      upvotes: Math.floor(Math.random() * 100),
+      review_id: uuid(),
+    };
+
+    const reviewString = JSON.stringify(newReview);
+
+    fs.writeFile(`./db/${newReview.product}.json`, reviewString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `Review for ${newReview.product} has been written to JSON file`
+          )
+    );
+
+    const response = {
+      status: 'success',
+      body: newReview,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
